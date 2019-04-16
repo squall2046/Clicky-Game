4824d01bc361bbed188f84b2d4ce029f12830689
squall2046 <squall2046@hotmail.com>
2019-04-16 17:38:31 -0400

    Update index.js

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 function Alert(props) {
   return <div className="alert">
-    <br/><h1 className="blink"> Game Over</h1>
-    <h3> Do you want to play again?</h3><br/>
+    <br/><h1> Game Over</h1>
+    <h3 className="blink"> Do you want to play again?</h3><br/>
     <div className="btn btn-danger replay" onClick={props.handleAlert} >Replay</div><br/><br/><br/>
 
     <div className="mGif">
